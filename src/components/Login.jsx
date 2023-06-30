e65aa53e632562ea337cd0799ae96d5fa4809661
abel34 <abel.dominguez@sw-argos.com>
2023-06-30 08:36:13 -0600

    [ADD] navigate to todo-list after login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,41 +1,73 @@
-import { useState } from "react"
-import { login } from "./functions/apiUser"
+import { useState } from "react";
+import { login } from "./functions/apiUser";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const loginAccount = async() => {
-        const body =  {
-            email:email,
-            password:password
-        }
-        const token = await login(body)
-        localStorage.setItem("token",token)
-        setEmail("")
-        setPassword("")
-    }
+  const navigate = useNavigate()
 
-    const handleClick = (event) => {
-        event.preventDefault()
-        loginAccount()
+  const loginAccount = async () => {
+    const body = {
+      email: email,
+      password: password,
+    };
+    try {
+      const token = await login(body);
+      localStorage.setItem("token", token);
+      setEmail("");
+      setPassword("");
+      navigate('/todos')
+    } catch (error) {
+        console.error(error)
     }
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    loginAccount();
+  };
   return (
     <div>
-        <form>
+      <form>
         <h3>Login</h3>
         <div className="input-group mb-3">
-            <span className="input-group-text" ><i className="bi bi-person"></i></span>
-            <input type="text" className="form-control" placeholder="Email" value={email} onChange={() => setEmail(event.target.value)} aria-label="Username" aria-describedby="basic-addon1"/>
+          <span className="input-group-text">
+            <i className="bi bi-person"></i>
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Email"
+            value={email}
+            onChange={() => setEmail(event.target.value)}
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+          />
         </div>
         <div className="input-group mb-3">
-            <span className="input-group-text"><i className="bi bi-unlock"></i></span>
-            <input type="text" className="form-control" placeholder="Password" value={password} onChange={() => setPassword(event.target.value)} aria-label="Password" aria-describedby="basic-addon1"/>
+          <span className="input-group-text">
+            <i className="bi bi-unlock"></i>
+          </span>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Password"
+            value={password}
+            onChange={() => setPassword(event.target.value)}
+            aria-label="Password"
+            aria-describedby="basic-addon1"
+          />
         </div>
-        <input type="submit" onClick={handleClick} className="btn btn-success" />
-        </form>
+        <input
+          type="submit"
+          onClick={handleClick}
+          className="btn btn-success"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
